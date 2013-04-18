a039cf7854a8b5c37a10f1fffe82a4eec6938593
rodrigodlima <rodrigo.lima@rodrigo.datacom>
2013-04-18 20:18:38 -0300

    Directory structure to build a OpenSSH package

---
+++ BUILD/openssh-5.4p1/version.h
@@ -0,0 +1,7 @@
+/* $OpenBSD: version.h,v 1.57 2010/03/07 22:01:32 djm Exp $ */
+
+#define SSH_VERSION	"OpenSSH_5.4"
+
+#define SSH_LPK		"lpk"
+#define SSH_PORTABLE	"p1"
+#define SSH_RELEASE	SSH_VERSION SSH_PORTABLE SSH_LPK
---
+++ SOURCES/openssh-5.4p1/version.h
@@ -0,0 +1,6 @@
+/* $OpenBSD: version.h,v 1.57 2010/03/07 22:01:32 djm Exp $ */
+
+#define SSH_VERSION	"OpenSSH_5.4"
+
+#define SSH_PORTABLE	"p1"
+#define SSH_RELEASE	SSH_VERSION SSH_PORTABLE
